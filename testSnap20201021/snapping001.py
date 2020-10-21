1610af1400e48697350d443bcd2c4d8415f5bb4a
KAZUO TSUTSUI <kzt206@gmail.com>
2020-10-21 21:47:09 +0900

    Create Snapping test program

---
+++ testSnap20201021/snapping001.py
@@ -0,0 +1,22 @@
+import numpy as np                        # fundamental package for scientific computing
+import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
+import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
+print("Environment Ready")
+
+# Setup:
+pipe = rs.pipeline()
+cfg = rs.config()
+cfg.enable_device_from_file("stairs.bag")
+profile = pipe.start(cfg)
+
+# Skip 5 first frames to give the Auto-Exposure time to adjust
+for x in range(5):
+    pipe.wait_for_frames()
+
+# Store next frameset for later processing:
+frameset = pipe.wait_for_frames()
+depth_frame = frameset.get_depth_frame()
+
+# Cleanup:
+pipe.stop()
+print("Frames Captured")
