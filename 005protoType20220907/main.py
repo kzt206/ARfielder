b5b295621ba413b84e53b929243fb7215385a562
kzt206 <kzt206@gmail.com>
2023-01-06 15:38:31 +0900

    create prototype folder

--- 005protoType20220907/main.py
+++ 005protoType20220907/main.py
@@ -28,6 +28,8 @@ figsizeHeight = 6
 
 colorMapType = 0
 
+q_scale=1
+
 figureContour = plt.figure(figsize=(figsizeWidth,figsizeHeight))
 
 #Configuration Update
@@ -60,6 +62,7 @@ def configLoad():
     figsizeWidth = configDic['figsizeWidth']
     figsizeHeight = configDic['figsizeHeight']
     colorMapType = configDic['colorMapType']
+    q_scale = configDic['q_scale']
 
 def main():
     dCamera = depthcamera.Depthcamera()
@@ -73,6 +76,10 @@ def main():
     configLoad()
 
     colormaps = ('jet_r','jet','prism','summer','terrain','terrain_r','ocean','ocean_r','gist_earth','gist_earth_r','rainbow','rainbow_r')
+
+    x=np.arange(0,640)
+    y=np.arange(0,480)
+
 
     #Begin Rendering
     try:
@@ -140,10 +147,10 @@ def main():
             # frameCenterVertical = 240
             # frameHeight = 120
             # frameWidth = 120
-            frameT = int(frameCenterVertical-frameHeight/2)
-            frameB = int(frameCenterVertical+frameHeight/2)
-            frameL = int(frameCenterHorizontal-frameWidth/2)
-            frameR = int(frameCenterHorizontal+frameWidth/2)
+            frameT = int(frameCenterVertical+frameHeight/2) #Top
+            frameB = int(frameCenterVertical-frameHeight/2) #Bottom
+            frameL = int(frameCenterHorizontal-frameWidth/2) #Left
+            frameR = int(frameCenterHorizontal+frameWidth/2) #Right
             cv2.rectangle(color_image, (frameL,frameT), (frameR,frameB), color, thickness=2)
 
             #put Grid
@@ -166,12 +173,11 @@ def main():
             # far =1200
             axContour.set_title("Display Depth:"+str(contourNear)+" - "+str(contourFar)+" [cm]\n"+"cmap:"+colormaps[colorMapType])
             # levelmapColor = np.linspace(contourNear,contourFar,8)
-            contourArray1 = np.arange(contourNear-40,contourNear,5)  # pitch mm
+            contourArray1 = np.arange(contourNear-50,contourNear,20)  # pitch mm
             contourArray2 = np.arange(contourNear,contourFar,5)  #pitch mm
-            contourArray3 = np.arange(contourFar,contourFar+40,5) #pitch mm
+            contourArray3 = np.arange(contourFar,contourFar+50,20) #pitch mm
             levelmapColor=np.append(contourArray1,contourArray2)
-            levelmapColor=np.append(levelmapColor,contourArray3) 
-            print(levelmapColor[:])
+            levelmapColor=np.append(levelmapColor,contourArray3)
             # levelmapColor = np.arange(contourNear,contourFar,30)
             # levelmapColor = [900,1100,1150,1200,1250,1300,1500]
             # bounds=np.linspace(contourNear,contourFar,8)
@@ -179,7 +185,7 @@ def main():
             levelmapContour = levelmapColor
             #axContour.text(200,200,textTime)
             axContour.set_xlim([frameL,frameR])
-            axContour.set_ylim([frameB,frameT])
+            axContour.set_ylim([frameT,frameB])
             #print(colormaps[colorMapType],colorMapType)
             
             ### colormap test ###
@@ -197,9 +203,46 @@ def main():
             # axContour.contour(filled_depth_values,levels=levelmapContour)
             # temporal filter hole filter
             cont1 = axContour.contourf(temporal_filter_values,cmap=colormaps[colorMapType],levels=levelmapColor,extend="both") #Color,levels=[0.75,0.8,0.85,0.9,0.95,1.,1.05]
+            
+            
+            #ベクトルの描画
+            # print("gradient")
+            (u,v)=np.gradient(temporal_filter_values)
+            print("frameL:%d, frameR:%d, frameB:%d, frameT:%d, temporal_filter_values:%s, u.shape:%s,v.shape:%s です" % (frameL,frameR,frameB,frameT,temporal_filter_values.shape,u.shape,v.shape)) 
+            #frameL:310, frameR:330, frameT:250, frameB:230, temporal_filter_values:(480, 640), u.shape:(480, 640),v.shape:(480, 640) です
+
+            # print("u",u[frameL:frameR])
+            # print("v",v[frameB:frameT])
+            
+            # print(frameB)
+            print(u[frameB:frameT,frameL:frameR])
+            # print(u[237:242,317:322])
+
+            u[:frameB,:]=0
+            u[frameT:,:]=0
+            u[:,:frameL]=0
+            u[:,frameR:]=0
+
+            v[:frameB,:]=0
+            v[frameT:,:]=0
+            v[:,:frameL]=0
+            v[:,frameR:]=0
+            
+            u[u>10],v[v>10] = 10, 10    # 勾配の上限下限
+            u[u<-10],v[v<-10] = -10, -10    # 勾配の上限下限
+
+
+            print(u[frameB:frameT,frameL:frameR])
+            # print(u[237:242,317:322])
+
+            # plt.quiver(-v,-u)
+            plt.quiver(x,y,v,u,angles='xy',scale=200)  #近くが高くて、遠くが低いので graident
+            # plt.quiver(x,y,-v,-u,angles='xy',scale=q_scale)
+            # print(temporal_filter_values.shape)
+
             cont1.cmap.set_under('pink')
             cbar = figureContour.colorbar(cont1,orientation="vertical",format="%3.2f") # カラーバーの表示
-            axContour.contour(temporal_filter_values,levels=levelmapContour)
+            # axContour.contour(temporal_filter_values,levels=levelmapContour)
             cbar.set_label('depth [mm]',size=12)
             # axContour.set_aspect(frameHeight/frameWidth)
             axContour.set_aspect('equal')
